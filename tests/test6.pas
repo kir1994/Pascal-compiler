b7bfaa0dc41249869fd48076f5a4c67de7e23e98
Icpecus <Icpecus@gmail.com>
2015-01-22 02:30:54 +0300

    типы integer, real, Boolean
    Объявление и задание значений.
    Не поняла, почему названия всех типов со строчной буквы, а у Boolean - с
    заглавной?!
    Парсер работает, остальное нет  :(

---
+++ tests/test6.pas
@@ -0,0 +1,16 @@
+program prog;
+const n = -10;
+  m = 1000000000;
+  b = true;
+  
+var
+    i: integer;
+    sum: Boolean;
+	kok: real;
+
+begin
+	i := -10;
+	kok := 12.075;
+	sum := True;
+	prog:=sum+sum
+end
